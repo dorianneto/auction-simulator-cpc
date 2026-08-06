fe8c27dff3cd12f5d3737a3aba375af46a2556d7
Dorian Neto <doriansampaioneto@gmail.com>
2026-08-06 15:27:48 -0230

    initial commit

---
+++ eslint.config.ts
@@ -0,0 +1,21 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.browser },
+    rules: {
+      quotes: ['error', 'single', { avoidEscape: true }],
+      eqeqeq: 'error',
+      'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      'prefer-const': 'error',
+      'no-var': 'error',
+    },
+  },
+  tseslint.configs.recommended,
+]);
